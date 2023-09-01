9ce93a13fecd2f0fd1088559afb69681be396506
l.phounphonh <l.phounphonh@proxiad.com>
2023-09-01 12:11:27 +0200

    ci: create header section, search section on HomePage

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RootNavigator from './src/navigators/RootNavigator';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, Theme, DefaultTheme } from '@react-navigation/native';
+import { useMemo } from 'react'
 
 export default function App() {
+  const theme: Theme = useMemo(
+    () => ({
+      ...DefaultTheme,
+      colors: {
+        ...DefaultTheme.colors,
+        background: "#F5F5F5",
+        text: "#191919",
+        border: "#D9D9D9",
+        primary: "#191919",
+      },
+    }),
+    []
+  );
+
   return (
     <View style={styles.container}>
-      <NavigationContainer>
+      <NavigationContainer theme={theme}>
         <RootNavigator />
-        <StatusBar style='dark' />
+        <StatusBar style="dark" />
       </NavigationContainer>
     </View>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,160 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import * as React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import * as React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useTheme } from "@react-navigation/native";
+import Icons from "@expo/vector-icons/MaterialIcons";
 
-const AVATAR = "https://ih1.redbubble.net/image.1671944269.9239/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg";
+const AVATAR =
+  "https://ih1.redbubble.net/image.1671944269.9239/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg";
 
 const HomeScreen = () => {
+  const { colors } = useTheme();
+
   return (
-    <ScrollView contentContainerStyle={{flexGrow: 1}}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24, flexDirection: 'row', alignItems:'center', gap: 8,}}>
-            <Image source={{
-                uri: AVATAR,
-            }} style={{ width: 60, aspectRatio: 1, borderRadius: 52}}
-             resizeMode='cover' />
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <SafeAreaView style={{ paddingVertical: 24, gap: 24 }}>
+        {/* header section */}
+        <View
+          style={{
+            padding: 24,
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 8,
+          }}
+        >
+          <Image
+            source={{
+              uri: AVATAR,
+            }}
+            style={{ width: 60, aspectRatio: 1, borderRadius: 52 }}
+            resizeMode="cover"
+          />
+          <View style={{ flex: 1 }}>
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "600",
+                marginBottom: 8,
+                color: colors.text,
+              }}
+              numberOfLines={1}
+            >
+              Hi, Brow !
+            </Text>
+            <Text
+              style={{
+                color: colors.text,
+                opacity: 0.75,
+              }}
+              numberOfLines={1}
+            >
+              Discorver fashion that suit your style
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={{
+              width: 52,
+              aspectRatio: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 50,
+              borderWidth: 1,
+              borderColor: colors.border,
+            }}
+          >
+            <Icons name="notifications" size={24} color={colors.text} />
+          </TouchableOpacity>
+        </View>
+        {/* Search section */}
+        <View style={{ flexDirection: "row", paddingHorizontal: 24, gap: 12 }}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              height: 52,
+              borderRadius: 52,
+              borderWidth: 1,
+              borderColor: colors.border,
+              alignItems: "center",
+              paddingHorizontal: 24,
+              gap: 12,
+              flexDirection: "row",
+            }}
+          >
+            <Icons
+              name="search"
+              size={24}
+              color={colors.text}
+              style={{ opacity: 0.5 }}
+            />
+            <Text
+              style={{
+                flex: 1,
+                fontSize: 16,
+                color: colors.text,
+                opacity: 0.5,
+              }}
+            >
+              Search
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: 52,
+              aspectRatio: 1,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 50,
+              backgroundColor: colors.primary,
+            }}
+          >
+            <Icons name="tune" size={24} color={colors.background} />
+          </TouchableOpacity>
+        </View>
+        {/* Grid Collection View */}
+        <View style={{ paddingHorizontal: 24 }}>
+          {/* Title Bar */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: "700",
+              }}
+            >
+              New Collection
+            </Text>
+            <TouchableOpacity>
+              <Text>See All</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            {/* Card */}
+            <View
+              style={{
+                flex: 1,
+                height: 200,
+                position: "relative",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                source={require("../assets/images/image-1.jpg")}
+                resizeMode="cover"
+                style={{
+                    flex:1
+                }}
+              />
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </ScrollView>
   );
-}
+};
 
-export default HomeScreen
+export default HomeScreen;
